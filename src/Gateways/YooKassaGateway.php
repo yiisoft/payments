75bdb16c367b2021180e963fbe52a91d7326703c
aleksey.bibeev <aleksey.bibeev@fixparts-online.com>
2025-11-19 06:35:38 +0000

    yookassa-draft

---
+++ src/Gateways/YooKassaGateway.php
@@ -0,0 +1,201 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Payments\Gateways;
+
+use Exception;
+use Yiisoft\Payments\Models\Customer;
+use Yiisoft\Payments\Models\PaymentIntent;
+use Yiisoft\Payments\Models\PaymentMethod;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Log\LoggerInterface;
+
+class YooKassaGateway extends AbstractGateway
+{
+    private string $shopId;
+    private string $secretKey;
+
+    public function __construct(
+        string $shopId,
+        string $secretKey,
+        ClientInterface $httpClient,
+        RequestFactoryInterface $requestFactory,
+        StreamFactoryInterface $streamFactory,
+        ?LoggerInterface $logger = null
+    ) {
+        parent::__construct($httpClient, $requestFactory, $streamFactory, $logger);
+        $this->shopId = $shopId;
+        $this->secretKey = $secretKey;
+    }
+
+    protected function getBaseUri(): string
+    {
+        return 'https://api.yookassa.ru/v3';
+    }
+
+    protected function createRequest(string $method, string $endpoint, array $data = [])
+    {
+        $request = parent::createRequest($method, $endpoint, $data);
+
+        $request = $request
+            ->withHeader('Authorization', $this->createAuthorizationHeader())
+            ->withHeader('Idempotence-Key', $this->createIdempotenceKey());
+
+        return $request;
+    }
+
+    public function createCustomer(Customer $customer): Customer
+    {
+        return $customer;
+    }
+
+    public function retrieveCustomer(string $customerId): Customer
+    {
+        throw new \RuntimeException('YooKassa API does not support retrieving customer');
+    }
+
+    public function updateCustomer(Customer $customer): Customer
+    {
+        throw new \RuntimeException('YooKassa API does not support updating customer');
+    }
+
+    public function deleteCustomer(string $customerId): void
+    {
+        throw new \RuntimeException('YooKassa API does not support delete customer');
+    }
+
+    public function createPaymentMethod(PaymentMethod $paymentMethod): PaymentMethod
+    {
+        return $paymentMethod;
+    }
+
+    public function attachPaymentMethod(string $paymentMethodId, string $customerId): PaymentMethod
+    {
+        return new PaymentMethod($paymentMethodId, 'yookassa', [], $customerId);
+    }
+
+    public function createPaymentIntent(PaymentIntent $intent): PaymentIntent
+    {
+        $data = [
+            'amount' => [
+                'value' => number_format($intent->amount / 100, 2, '.', ''),
+                'currency' => strtoupper($intent->currency ?? 'USD'),
+            ],
+            'capture' => false,
+            'confirmation' => [
+                'type' => 'redirect',
+                'return_url' => $intent->metadata['return_url'] ?? ''
+            ],
+            'description' => $intent->description,
+            'metadata' => $intent->metadata,
+        ];
+
+        if ($intent->paymentMethodId !== null) {
+            $data['payment_method_data'] = [
+                'type' => $intent->paymentMethodId,
+            ];
+        }
+
+        $request = $this->createRequest('POST', '/payments', $data);
+        $response = $this->sendRequest($request);
+
+        return $this->parsePaymentIntent($response);
+    }
+
+    public function retrievePaymentIntent(string $intentId): PaymentIntent
+    {
+        $request = $this->createRequest('GET', "/payments/{$intentId}");
+        $response = $this->sendRequest($request);
+
+        return $this->parsePaymentIntent($response);
+    }
+
+    public function confirmPaymentIntent(string $intentId, array $params = []): PaymentIntent
+    {
+        return $this->capturePaymentIntent($intentId,  $params);
+    }
+
+    public function capturePaymentIntent(string $intentId, array $params = []): PaymentIntent
+    {
+        $request = $this->createRequest('POST', "/payments/{$intentId}/capture", []);
+        $response = $this->sendRequest($request);
+
+        return $this->parsePaymentIntent($response);
+    }
+
+    public function cancelPaymentIntent(string $intentId, array $params = []): PaymentIntent
+    {
+        $request = $this->createRequest('POST', "/payments/{$intentId}/cancel", $params);
+        $response = $this->sendRequest($request);
+
+        return $this->parsePaymentIntent($response);
+    }
+
+    public function createRefund(string $paymentId, array $params = []): array
+    {
+        $data = [
+            'payment_id' => $paymentId,
+            'amount' => $this->createAmountValue($params['amount'], $params['currency']),
+            'description' => $params['description'] ?? null,
+        ];
+
+        $response = $this->sendRequest($this->createRequest('POST', '/refunds', $data));
+        $amount = $this->parseAmountValue($response);
+
+        return [
+            'id' => $response['id'],
+            'payment_id' => $response['payment_id'],
+            'status' => $response['status'],
+            'created_at' => $response['created_at'],
+            'amount' => $amount['value'],
+            'currency' => $amount['currency']
+        ];
+    }
+
+    protected function createIdempotenceKey()
+    {
+        return uniqid('yk_', true);
+    }
+
+    protected function createAuthorizationHeader()
+    {
+        return "Basic " . base64_encode("{$this->shopId}:{$this->secretKey}");
+    }
+
+    protected function createAmountValue(int $amount, string $currency)
+    {
+        return [
+            'value' => number_format($amount / 100, 2, '.', ''),
+            'currency' => strtoupper($currency),
+        ];
+    }
+
+    protected function parseAmountValue(array $response)
+    {
+        return [
+            'value' => (int) ($response['amount']['value'] * 100),
+            'currency' => strtolower($response['amount']['currency']),
+        ];
+    }
+
+    protected function parsePaymentIntent(array $response): PaymentIntent
+    {
+        $amount = $this->parseAmountValue($response);
+
+        return new PaymentIntent(
+            id: $response['id'],
+            status: $response['status'],
+            amount: $amount['value'],
+            currency: $amount['currency'],
+            paymentMethodId: $response['payment_method']['id'] ?? null,
+            description: $response['description'] ?? null,
+            createdAt: strtotime($response['created_at']),
+            metadata: [
+                'confirmation_url' => $response['confirmation']['confirmation_url'] ?? null
+            ]
+        );
+    }
+}
---
+++ tests/Gateways/YooKassaGatewayTest.php
@@ -0,0 +1,196 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Payments\Tests\Gateways;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Payments\Models\PaymentIntent;
+use Yiisoft\Payments\Gateways\YooKassaGateway;
+use Yiisoft\Payments\Tests\Support\TestHttpClient;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Log\NullLogger;
+
+class YooKassaGatewayTest extends TestCase
+{
+    private TestHttpClient $httpClient;
+    private Psr17Factory $psr17Factory;
+    private YooKassaGateway $gateway;
+
+    protected function setUp(): void
+    {
+        $this->psr17Factory = new Psr17Factory();
+        $this->httpClient = new TestHttpClient($this->psr17Factory);
+
+        $this->gateway = new YooKassaGateway(
+            'shop_ip',
+            'secret_key',
+            $this->httpClient,
+            $this->psr17Factory,
+            $this->psr17Factory,
+            new NullLogger()
+        );
+    }
+
+    private function withResponse(array $response): void
+    {
+        $this->httpClient->setNextResponse($response);
+    }
+
+    private function getLastRequest(): array
+    {
+        return $this->httpClient->lastRequest;
+    }
+
+    public function testCreateCustomer(): void {}
+
+    public function testCreatePaymentIntent(): void
+    {
+        $paymentIntent = new PaymentIntent(
+            null,
+            null,
+            1000,
+            'rub',
+            'cus_test123',
+            'bank_card',
+            null,
+            'Test payment',
+            ['order_id' => '12345'],
+            null,
+            null,
+            true,
+            false,
+            false,
+            'test@example.com',
+            'TEST'
+        );
+
+        $this->withResponse([
+            "id" => "30ae77b9-000f-5001-8000-13e0de458932",
+            "status" => "pending",
+            "amount" => [
+                "value" => "100.00",
+                "currency" => "RUB",
+            ],
+            "description" => "Test payment",
+            "recipient" => [
+                "account_id" => "1183512",
+                "gateway_id" => "2557477",
+            ],
+            "payment_method" => [
+                "type" => "bank_card",
+                "id" => "30ae77b9-000f-5001-8000-13e0de458932",
+                "saved" => false,
+                "status" => "inactive",
+            ],
+            "created_at" => "2025-11-18T12:18:01.563Z",
+            "confirmation" => [
+                "type" => "redirect",
+                "return_url" => "https://5ebde85d10de.ngrok-free.app?payment=test_1",
+                "confirmation_url" => "https://yoomoney.ru/checkout/payments/v2/contract?orderId=30ae77b9-000f-5001-8000-13e0de458932",
+            ],
+            "test" => true,
+            "paid" => false,
+            "refundable" => false,
+            "metadata" => [],
+        ]);
+
+        $result = $this->gateway->createPaymentIntent($paymentIntent);
+
+        $this->assertSame('30ae77b9-000f-5001-8000-13e0de458932', $result->id);
+        $this->assertSame(10000, $result->amount);
+        $this->assertSame('rub', $result->currency);
+        $this->assertSame('Test payment', $result->description);
+    }
+
+    public function testConfirmPaymentIntent(): void
+    {
+        $this->withResponse([
+            "id" => "30ae77b9-000f-5001-8000-13e0de458932",
+            "status" => "succeeded",
+            "amount" => [
+                "value" => "100.00",
+                "currency" => "RUB",
+            ],
+            "income_amount" => [
+                "value" => "96.50",
+                "currency" => "RUB",
+            ],
+            "description" => "Order 1",
+            "recipient" => [
+                "account_id" => "1183512",
+                "gateway_id" => "2557477",
+            ],
+            "payment_method" => [
+                "type" => "bank_card",
+                "id" => "30af50eb-000f-5001-8000-1533ca71a452",
+                "saved" => false,
+                "status" => "inactive",
+                "title" => "Bank card *4477",
+                "card" => [
+                    "first6" => "555555",
+                    "last4" => "4477",
+                    "expiry_year" => "2011",
+                    "expiry_month" => "11",
+                    "card_type" => "MasterCard",
+                    "card_product" => [
+                        "code" => "E",
+                    ],
+                    "issuer_country" => "US",
+                ],
+            ],
+            "captured_at" => "2025-11-19T03:51:05.783Z",
+            "created_at" => "2025-11-19T03:44:43.200Z",
+            "test" => true,
+            "refunded_amount" => [
+                "value" => "0.00",
+                "currency" => "RUB",
+            ],
+            "paid" => true,
+            "refundable" => true,
+            "metadata" => [],
+            "authorization_details" => [
+                "rrn" => "166103673691924",
+                "auth_code" => "758053",
+                "three_d_secure" => [
+                    "applied" => true,
+                    "protocol" => "v1",
+                    "method_completed" => false,
+                    "challenge_completed" => true,
+                ],
+            ],
+        ]);
+
+        $result = $this->gateway->confirmPaymentIntent('30ae77b9-000f-5001-8000-13e0de458932', ['return_url' => 'https://example.com/return']);
+
+        $this->assertSame('30ae77b9-000f-5001-8000-13e0de458932', $result->id);
+        $this->assertSame('succeeded', $result->status);
+    }
+
+    public function testCreateRefund(): void
+    {
+        $this->withResponse([
+            "id" => "30af6093-0015-5001-8000-196e1cbaceef",
+            "payment_id" => "30af50eb-000f-5001-8000-1533ca71a452",
+            "status" => "succeeded",
+            "created_at" => "2025-11-19T04:51:31.067Z",
+            "amount" => [
+                "value" => "100.00",
+                "currency" => "RUB"
+            ],
+            "refund_authorization_details" => [
+                "rrn" => "325981525210676"
+            ]
+        ]);
+
+        $result = $this->gateway->createRefund('30af50eb-000f-5001-8000-1533ca71a452', [
+            'amount' => 1000,
+            'currency' => 'rub'
+        ]);
+
+        $this->assertSame("30af6093-0015-5001-8000-196e1cbaceef", $result['id']);
+        $this->assertSame(10000, $result['amount']);
+        $this->assertSame('rub', $result['currency']);
+        $this->assertSame('succeeded', $result['status']);
+    }
+}
