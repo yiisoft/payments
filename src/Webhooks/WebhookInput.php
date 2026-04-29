ad840e6bf9af6294b27414f05d39194bb125a3ff
Stanislav Viazovetskov <mob.stuv@gmail.com>
2026-04-29 01:09:08 +0000

    Add params to webhook input

--- src/Webhooks/WebhookInput.php
+++ src/Webhooks/WebhookInput.php
@@ -11,10 +11,14 @@ final readonly class WebhookInput
 {
     /**
      * @param array<string, list<string>> $headers
+     * @param array<string, mixed> $queryParams
+     * @param array<string, mixed> $bodyParams
      */
     public function __construct(
         public string $rawBody,
         public array $headers = [],
+        public array $queryParams = [],
+        public array $bodyParams = [],
     ) {
     }
 }
