a462c70d781d6c4812e6e9ae13397941f9a1e310
Stanislav Viazovetskov <mob.stuv@gmail.com>
2026-05-30 12:58:19 +0000

    Add Robokassa webhook recognition unit tests

--- tests/Webhooks/WebhookRobokassaEventRecognizerTest.php
+++ tests/Webhooks/WebhookRobokassaEventRecognizerTest.php
@@ -48,6 +48,52 @@ final class WebhookRobokassaEventRecognizerTest extends TestCase
         $this->assertSame(WebhookEventType::PaymentSucceeded, $recognizer->recognizeEventType($providerEventType));
     }
 
+    public function testRecognizesSupportedCallbackWhenRequiredParamsAreDuplicatedWithSameValues(): void
+    {
+        $recognizer = new WebhookRobokassaEventRecognizer();
+
+        $providerEventType = $recognizer->recognizeProviderEventType(new WebhookInput(
+            queryParams: [
+                'OutSum' => '100.00',
+                'InvId' => '123',
+                'SignatureValue' => 'abc123',
+            ],
+            bodyParams: [
+                'OutSum' => '100.00',
+                'InvId' => '123',
+                'SignatureValue' => 'abc123',
+            ],
+        ));
+
+        $this->assertSame(WebhookRobokassaCallbackFormat::CALLBACK_TYPE, $providerEventType);
+        $this->assertSame(WebhookEventType::PaymentSucceeded, $recognizer->recognizeEventType($providerEventType));
+    }
+
+    public function testRecognizesSupportedCallbackWithCustomProviderFields(): void
+    {
+        $recognizer = new WebhookRobokassaEventRecognizer();
+
+        $providerEventType = $recognizer->recognizeProviderEventType(new WebhookInput(queryParams: [
+            'OutSum' => '100.00',
+            'InvId' => '123',
+            'SignatureValue' => 'abc123',
+            'Shp_orderId' => 'order-123',
+        ]));
+
+        $this->assertSame(WebhookRobokassaCallbackFormat::CALLBACK_TYPE, $providerEventType);
+        $this->assertSame(WebhookEventType::PaymentSucceeded, $recognizer->recognizeEventType($providerEventType));
+    }
+
+    public function testReturnsNullForInputWithoutCallbackParams(): void
+    {
+        $recognizer = new WebhookRobokassaEventRecognizer();
+
+        $this->assertNull($recognizer->recognizeProviderEventType(new WebhookInput(
+            rawBody: '{"OutSum":"100.00","InvId":"123","SignatureValue":"abc123"}',
+            headers: ['Content-Type' => 'application/json'],
+        )));
+    }
+
     public function testReturnsNullForMissingRequiredCallbackParameter(): void
     {
         $recognizer = new WebhookRobokassaEventRecognizer();
