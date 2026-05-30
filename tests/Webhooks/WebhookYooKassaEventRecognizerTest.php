5a9b19e9395c3926ced7927caa2612128d9fe350
Stanislav Viazovetskov <mob.stuv@gmail.com>
2026-05-30 12:04:09 +0000

    Add YooKassa webhook recognition unit tests

--- tests/Webhooks/WebhookYooKassaEventRecognizerTest.php
+++ tests/Webhooks/WebhookYooKassaEventRecognizerTest.php
@@ -58,6 +58,36 @@ final class WebhookYooKassaEventRecognizerTest extends TestCase
         $this->assertSame($expectedEventType, $recognizer->recognizeEventType($providerEventType));
     }
 
+    #[DataProvider('basicPaymentEventTypesProvider')]
+    public function testRecognizesProviderAndNormalizedEventTypeFromYooKassaPayload(
+        string $providerEventType,
+        WebhookEventType $expectedEventType,
+    ): void {
+        $recognizer = new WebhookYooKassaEventRecognizer();
+        $input = new WebhookInput(rawBody: json_encode(['event' => $providerEventType], JSON_THROW_ON_ERROR));
+
+        $recognizedProviderEventType = $recognizer->recognizeProviderEventType($input);
+
+        $this->assertSame($providerEventType, $recognizedProviderEventType);
+        $this->assertSame($expectedEventType, $recognizer->recognizeEventType($recognizedProviderEventType));
+    }
+
+    #[DataProvider('invalidYooKassaPayloadProvider')]
+    public function testReturnsNullForInvalidYooKassaPayloadWithoutException(string $rawBody): void
+    {
+        $recognizer = new WebhookYooKassaEventRecognizer();
+
+        $this->assertNull($recognizer->recognizeProviderEventType(new WebhookInput(rawBody: $rawBody)));
+    }
+
+    public function testReturnsNullForUnknownProviderEventType(): void
+    {
+        $recognizer = new WebhookYooKassaEventRecognizer();
+
+        $this->assertNull($recognizer->recognizeEventType('payment.expired'));
+        $this->assertNull($recognizer->recognizeEventType('payout.succeeded'));
+    }
+
     /**
      * @return iterable<string, array{string, WebhookEventType}>
      */
@@ -67,5 +97,18 @@ final class WebhookYooKassaEventRecognizerTest extends TestCase
         yield 'succeeded' => ['payment.succeeded', WebhookEventType::PaymentSucceeded];
         yield 'canceled' => ['payment.canceled', WebhookEventType::PaymentCanceled];
         yield 'refunded' => ['refund.succeeded', WebhookEventType::PaymentRefunded];
+    }
+
+    /**
+     * @return iterable<string, array{string}>
+     */
+    public static function invalidYooKassaPayloadProvider(): iterable
+    {
+        yield 'malformed json' => ['{"event":"payment.succeeded"'];
+        yield 'json list' => ['[]'];
+        yield 'json null' => ['null'];
+        yield 'missing event' => ['{"type":"notification"}'];
+        yield 'null event' => ['{"event":null}'];
+        yield 'object event' => ['{"event":{"name":"payment.succeeded"}}'];
     }
 }
