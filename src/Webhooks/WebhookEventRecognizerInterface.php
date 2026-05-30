c7ce4ceb2f702047733d8ebf78a310f8c4c7267e
Stanislav Viazovetskov <mob.stuv@gmail.com>
2026-05-30 10:59:01 +0000

    Document raw provider webhook event type contract

--- src/Webhooks/WebhookEventRecognizerInterface.php
+++ src/Webhooks/WebhookEventRecognizerInterface.php
@@ -9,6 +9,13 @@ namespace Yiisoft\Payments\Webhooks;
  */
 interface WebhookEventRecognizerInterface
 {
+    /**
+     * Returns the raw provider event type as it appears in the incoming webhook request.
+     *
+     * The returned value must be the provider-defined event name/code without mapping it
+     * to a library-level {@see WebhookEventType}. Return null when the request does not
+     * contain an event type that can be recognized by this provider.
+     */
     public function recognizeProviderEventType(WebhookInput $input): ?string;
 
     public function recognizeEventType(string $providerEventType): ?WebhookEventType;
